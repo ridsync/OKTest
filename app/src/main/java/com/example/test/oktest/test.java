7367882db989de03b856327e69450ad3f20447d1
ojungwon <ridsync@gmail.com>
2014-09-29 13:51:16 +0900

    test commit

---
+++ app/src/main/java/com/example/test/oktest/test.java
@@ -0,0 +1,15 @@
+package com.example.test.oktest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ojungwon on 2014-09-29.
+ */
+public class test extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
