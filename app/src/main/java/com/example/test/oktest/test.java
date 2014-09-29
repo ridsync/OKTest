e128d932eff37bd1b3a64622e0dba1008a94509d
ojungwon <ridsync@gmail.com>
2014-09-29 14:31:21 +0900

    test commit 22

--- app/src/main/java/com/example/test/oktest/test.java
+++ app/src/main/java/com/example/test/oktest/test.java
@@ -1,6 +1,7 @@
 package com.example.test.oktest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
@@ -11,5 +12,11 @@ public class test extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
